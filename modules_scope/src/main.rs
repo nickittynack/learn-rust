82e291105728ebd895eaf0855c63d8ab948ea14a
Nick Reynolds <nicholas.reynolds@me.com>
2018-12-30 17:25:46 +1100

    modules

---
+++ modules_scope/src/main.rs
@@ -0,0 +1,15 @@
+mod sound {
+    pub mod instrument {
+        pub fn clarinet() {
+            // Function body code goes here
+        }
+    }
+}
+
+use crate::sound::instrument::clarinet;
+
+fn main() {
+    clarinet();
+    clarinet();
+    clarinet();
+}
